c4bd9de55bf8f9a8ef991fac14b5647f6cabc6f7
Asif Mohammed <asifm42@gmail.com>
2017-11-22 12:24:29 -0600

    Adding comments to config variables

--- config/scormcloud.php
+++ config/scormcloud.php
@@ -11,10 +11,61 @@ return [
     |
     */
 
-    'url' => env('SCORMCLOUD_URL','https://cloud.scorm.com/EngineWebServices'),
-    'app_id' => env('SCORMCLOUD_APPID',''),
-    'key' => env('SCORMCLOUD_KEY',''),
-    'organization' => env('SCORMCLOUD_ORGANIZATION',''),
-    'app_name' => env('SCORMCLOUD_APP_NAME',''),
-    'version' => env('SCORMCLOUD_VERSION','')
+    /*
+     * The scorm engine service url.
+     *
+     * This attribute is a relic of a previous Rustici ScormCloud product
+     * iteration. It will most likely not need to be overriden. But its
+     * there for instances such as when HTTPS cannot be supported, etc.
+     */
+
+    'url' => env('SCORMCLOUD_URL', 'https://cloud.scorm.com/EngineWebServices'),
+
+    /*
+     * The application id
+     *
+     * Required
+     */
+
+    'app_id' => env('SCORMCLOUD_APPID', ''),
+
+    /*
+     * Any secret key associated with the app id
+     *
+     * Required
+     */
+
+    'key' => env('SCORMCLOUD_KEY', ''),
+
+    /*
+     * The organization name.
+     *
+     * This will be used to create an origin string which is useful
+     * for debugging on the SCORM Cloud developers’ side
+     *
+     * optional
+     */
+    'organization' => env('SCORMCLOUD_ORGANIZATION', 'scl'),
+
+    /*
+     * The organization name.
+     *
+     * This will be used to create an origin string which is useful
+     * for debugging on the SCORM Cloud developers’ side
+     *
+     * optional
+     */
+
+    'app_name' => env('SCORMCLOUD_APP_NAME', 'app'),
+
+    /*
+     * The app name.
+     *
+     * This will be used to create an origin string which is useful
+     * for debugging on the SCORM Cloud developers’ side
+     *
+     * optional
+     */
+
+    'version' => env('SCORMCLOUD_VERSION','1.0')
 ];
